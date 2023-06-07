33e20dcc89eefc537a6c73cdea1650012dbca8f2
kwiacek175 <259378@student.pwr.edu.pl>
2023-06-07 03:11:47 +0200

    Further modifications

--- WeatherAPI_Project/src/main/java/com/example/weatherapi_project/WeatherApiProjectApplication.java
+++ WeatherAPI_Project/src/main/java/com/example/weatherapi_project/WeatherApiProjectApplication.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.awt.Desktop;
+import java.awt.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.net.URI;
 import java.util.List;
 
@@ -31,15 +33,18 @@ public class WeatherApiProjectApplication {
             List<WeatherDescription> weatherDescriptions = response.getWeather();
             MainWeatherData mainWeatherData = response.getMain();
             String weather = response.getWeather().get(0).getDescription();
-            double temperature = response.getMain().getTemp();
+            double temperature = response.getMain().getTemp() - 273.15;
+            temperature = Math.round(temperature * 100.0)/100.0;
             int humidity = response.getMain().getHumidity();
             double pressure = mainWeatherData.getPressure();
             double windSpeed = response.getWind().getSpeed();
-            double rainfall = response.getRainfall().getOneHour();
+            RainfallData rainfallData = response.getRainfall();
+            double rainfall = (rainfallData != null) ? rainfallData.getOneHour() : 0.0;
+
 
             String output = "Weather in " + city + ": " + weather + "\n";
             output += "Temperature: " + temperature + "°C\n";
-            output += "Humidity: " + humidity + "%";
+            output += "Humidity: " + humidity + "%\n";
             output += "Pressure: " + pressure + " hPa\n";
             output += "Wind Speed: " + windSpeed + " m/s\n";
             output += "Rainfall (1 hour): " + rainfall + " mm";
@@ -55,8 +60,19 @@ public class WeatherApiProjectApplication {
     }
     public static void openWebpage(String url) {
         try {
+            if (GraphicsEnvironment.isHeadless()) {
+                System.out.println("Running in headless mode. Unable to open webpage.");
+                return;
+            }
+
+            String htmlContent = "<html><head><title>Pogoda we Wrocławiu!</title></head><body><h1 style='text-align:center;'>Pogoda we Wrocławiu!</h1><p>API pogodowe: " + url + "</p></body></html>";
+            String tempFileName = "temp.html";
+            BufferedWriter writer = new BufferedWriter(new FileWriter(tempFileName));
+            writer.write(htmlContent);
+            writer.close();
+
             Desktop desktop = Desktop.getDesktop();
-            desktop.browse(new URI(url));
+            desktop.browse(new URI("file://" + tempFileName));
         } catch (Exception e) {
             e.printStackTrace();
         }
